2cc397d5a68d01b85517a839132235e37d4cde7e
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-09 22:56:03 -0300

    classe Endereco criada, com validação do CEP e lógica de PAÍS/ESTADO/CIDADE criada

---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/model/Endereco.java
@@ -0,0 +1,86 @@
+package io.github.viniciusnunes01.ticketmastersystem.model;
+
+public class Endereco {
+
+	private Integer idEndereco;
+	private String logradouro;
+	private String numero;
+	private String bairro;
+	private String cep;
+	private String complemento;
+	private Cidade cidade;
+
+	public Endereco(String logradouro, String numero, String bairro, String cep, String complemento, Cidade cidade) {
+		super();
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cep = cep.replace("-", "");
+		if(this.cep.length() != 8) {
+			throw new IllegalArgumentException("O CEP deve conter 8 dígitos.");
+		}
+		this.complemento = complemento;
+		
+		if(cidade == null) {
+			throw new IllegalArgumentException("É necessário uma cidade para esse endereço");
+		}
+		this.cidade = cidade;
+	}
+
+	public Integer getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+}
