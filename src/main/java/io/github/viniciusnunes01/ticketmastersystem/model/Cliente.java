553a932baf029d9db9c952cc1999ceddf0009d88
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-09 21:45:57 -0300

    create Cliente model with personal and address data

---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/model/Cliente.java
@@ -0,0 +1,102 @@
+package io.github.viniciusnunes01.ticketmastersystem.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Cliente {
+
+	private Integer idCliente;
+	private String cpf;
+	private LocalDate dataNascimento;
+	private String telefone;
+	private String email;
+	private String endereco;
+	private String cidade;
+	private String estado;
+	private String pais;
+
+	public Cliente(String cpf, LocalDate dataNascimento, String telefone, String email, String endereco, String cidade,
+			String estado, String pais) {
+		super();
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.telefone = telefone;
+		this.email = email;
+		this.endereco = endereco;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.pais = pais;
+	}
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+}
