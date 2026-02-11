79979b606b0369cee7c9d519e895e7cd14a5ddfe
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-10 21:08:08 -0300

    criação do service do pais

---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/service/PaisService.java
@@ -0,0 +1,20 @@
+package io.github.viniciusnunes01.ticketmastersystem.service;
+
+import org.springframework.stereotype.Service;
+
+import io.github.viniciusnunes01.ticketmastersystem.model.Pais;
+import io.github.viniciusnunes01.ticketmastersystem.repository.PaisRepository;
+
+@Service
+public class PaisService {
+
+	private final PaisRepository paisRepository;
+
+	public PaisService(PaisRepository paisRepository) {
+		this.paisRepository = paisRepository;
+	}
+
+	public Pais salvar(Pais pais) {
+		return paisRepository.save(pais);
+	}
+}
