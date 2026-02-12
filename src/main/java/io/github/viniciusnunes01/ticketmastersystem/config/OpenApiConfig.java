e52867d9d8562a3da8787a18bfdf39dc50e3e700
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-12 16:32:05 -0300

    swagger add

---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package io.github.viniciusnunes01.ticketmastersystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TicketMaster System API")
+                        .version("1.0")
+                        .description("API desenvolvida para gestão de vendas de ingressos de grandes eventos.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
