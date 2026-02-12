7bed7e0dcba4cb44192c4e5a6ba2643a7f37ee0b
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-11 23:07:22 -0300

    evento controller e service criados, com validações

---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/controller/EventoController.java
@@ -0,0 +1,45 @@
+package io.github.viniciusnunes01.ticketmastersystem.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.viniciusnunes01.ticketmastersystem.model.Evento;
+import io.github.viniciusnunes01.ticketmastersystem.service.EventoService;
+
+@RestController
+@RequestMapping("/evento")
+public class EventoController {
+
+	private final EventoService eventoService;
+
+	public EventoController(EventoService eventoService) {
+		this.eventoService = eventoService;
+	}
+
+	@PostMapping
+	public Evento cadastrarEvento(@RequestBody Evento evento) {
+		return eventoService.salvar(evento);
+	}
+
+	@GetMapping
+	public List<Evento> listarEvento() {
+		return eventoService.listarTodos();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Evento> buscarPorId(@PathVariable Integer id) {
+		try {
+			Evento evento = eventoService.buscarPorId(id);
+			return ResponseEntity.ok(evento);
+		} catch (RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+}
---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/service/EventoService.java
@@ -0,0 +1,31 @@
+package io.github.viniciusnunes01.ticketmastersystem.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.github.viniciusnunes01.ticketmastersystem.model.Evento;
+import io.github.viniciusnunes01.ticketmastersystem.repository.EventoRepository;
+
+@Service
+public class EventoService {
+
+	private final EventoRepository eventoRepository;
+
+	public EventoService(EventoRepository eventoRepository) {
+		this.eventoRepository = eventoRepository;
+	}
+
+	public Evento salvar(Evento evento) {
+		return eventoRepository.save(evento);
+	}
+
+	public List<Evento> listarTodos() {
+		return eventoRepository.findAll();
+	}
+
+	public Evento buscarPorId(Integer id) {
+		return eventoRepository.findById(id).orElseThrow(() -> new RuntimeException("Evento não encontrado!"));
+	}
+
+}
