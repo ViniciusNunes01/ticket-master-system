5bead4cf00431cb786a4b37ff28992875384f000
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-11 23:08:10 -0300

    atualização na carga de dados para testes

--- src/main/java/io/github/viniciusnunes01/ticketmastersystem/CargaDeDados.java
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/CargaDeDados.java
@@ -1,47 +1,76 @@
 package io.github.viniciusnunes01.ticketmastersystem;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.github.viniciusnunes01.ticketmastersystem.model.Cidade;
+import io.github.viniciusnunes01.ticketmastersystem.model.Cliente;
+import io.github.viniciusnunes01.ticketmastersystem.model.Endereco;
 import io.github.viniciusnunes01.ticketmastersystem.model.Estado;
+import io.github.viniciusnunes01.ticketmastersystem.model.Evento;
 import io.github.viniciusnunes01.ticketmastersystem.model.Pais;
 import io.github.viniciusnunes01.ticketmastersystem.repository.CidadeRepository;
+import io.github.viniciusnunes01.ticketmastersystem.repository.ClienteRepository;
 import io.github.viniciusnunes01.ticketmastersystem.repository.EstadoRepository;
+import io.github.viniciusnunes01.ticketmastersystem.repository.EventoRepository;
 import io.github.viniciusnunes01.ticketmastersystem.repository.PaisRepository;
 
 @Configuration
 public class CargaDeDados {
 
-	@Bean
-	CommandLineRunner executarCargaInicial(PaisRepository paisRepository, EstadoRepository estadoRepository,
-			CidadeRepository cidadeRepository) {
+    @Bean
+    CommandLineRunner executarCargaInicial(
+            PaisRepository paisRepository,
+            EstadoRepository estadoRepository,
+            CidadeRepository cidadeRepository,
+            ClienteRepository clienteRepository,
+            EventoRepository eventoRepository
+    ) {
+        return args -> {
+            if (paisRepository.count() == 0) {
+                
+                var brasil = new Pais("Brasil", "BR");
+                paisRepository.save(brasil);
 
-		return args -> {
+                var saoPauloEstado = new Estado("São Paulo", "SP", brasil);
+                estadoRepository.save(saoPauloEstado);
 
-			paisRepository.deleteAll();
-			estadoRepository.deleteAll();
+                var jundiai = new Cidade("Jundiaí", saoPauloEstado);
+                cidadeRepository.save(jundiai);
 
-			var brasil = new Pais("Brasil", "BR");
-			brasil = paisRepository.save(brasil);
+                var cliente = new Cliente();
+                cliente.setNome("Vinicius Nunes");
+                cliente.setCpf("123.456.789-00");
+                cliente.setEmail("vinicius@email.com");
+                cliente.setTelefone("(11) 99999-9999");
+                cliente.setDataNascimento(java.time.LocalDate.of(2000, 1, 1));
+                
+                var endereco = new Endereco();
+                endereco.setLogradouro("Av. Paulista");
+                endereco.setNumero("1000");
+                endereco.setBairro("Bela Vista");
+                endereco.setCep("01310-100");
+                endereco.setComplemento("Apto 50");
+                endereco.setCidade(jundiai);
+                
+                cliente.setEndereco(endereco);
+                clienteRepository.save(cliente);
 
-			var eua = new Pais("Estados Unidos", "US");
-			eua = paisRepository.save(eua);
+                var rockInRio = new Evento();
+                rockInRio.setNome("Rock in Rio 2026");
+                rockInRio.setData(LocalDateTime.of(2026, 9, 20, 20, 0));
+                rockInRio.setCapacidadeMaxima(100);
+                rockInRio.setIngressosRestantes(100);
+                rockInRio.setPrecoBase(new BigDecimal("550.00"));
+                
+                eventoRepository.save(rockInRio);
 
-			var saoPaulo = new Estado("São Paulo", "SP", brasil);
-			var rio = new Estado("Rio de Janeiro", "RJ", brasil);
-			var florida = new Estado("Florida", "FL", eua);
-
-			saoPaulo = estadoRepository.save(saoPaulo);
-			rio = estadoRepository.save(rio);
-			florida = estadoRepository.save(florida);
-
-			var jundiai = new Cidade("Jundiaí", saoPaulo);
-			cidadeRepository.save(jundiai);
-
-			System.out.println("sucesso!");
-		};
-	}
-
+                System.out.println("✅ Carga inicial COMPLETA: Cliente (ID 1) e Evento (ID 1) criados!");
+            }
+        };
+    }
 }
