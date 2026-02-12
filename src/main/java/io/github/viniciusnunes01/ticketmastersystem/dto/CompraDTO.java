12ab91f412d2baf52b0316eb8cae2e50bc15e39d
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-11 23:08:01 -0300

    DTO para auxiliar na compra do ingresso

---
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/dto/CompraDTO.java
@@ -0,0 +1,23 @@
+package io.github.viniciusnunes01.ticketmastersystem.dto;
+
+public class CompraDTO {
+
+	private Integer idCliente;
+	private Integer idEvento;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public Integer getIdEvento() {
+		return idEvento;
+	}
+
+	public void setIdEvento(Integer idEvento) {
+		this.idEvento = idEvento;
+	}
+}
