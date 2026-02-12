3749139a7a2b7c67f5df56ecb287ff9b67893436
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-12 14:49:24 -0300

    função para validação

--- src/main/java/io/github/viniciusnunes01/ticketmastersystem/repository/IngressoRepository.java
+++ src/main/java/io/github/viniciusnunes01/ticketmastersystem/repository/IngressoRepository.java
@@ -3,7 +3,12 @@ package io.github.viniciusnunes01.ticketmastersystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.viniciusnunes01.ticketmastersystem.model.Ingresso;
+import io.github.viniciusnunes01.ticketmastersystem.model.Cliente;
+import io.github.viniciusnunes01.ticketmastersystem.model.Evento;
+
+
 
 public interface IngressoRepository extends JpaRepository<Ingresso, Integer> {
-
+	
+	long countByClienteAndEvento(Cliente cliente, Evento evento);
 }
